180997b775f8190890344e38ed051c7463783b10
Mufil Rahman A <138945614+rahmanizum@users.noreply.github.com>
2023-08-17 01:40:02 +0530

    Add files via upload
    1st bootsrap registration form

---
+++ main.js
@@ -0,0 +1,8 @@
+function print(){
+    const name=document.getElementById("name").value;
+    const email=document.getElementById("email").value;
+    const phone=document.getElementById("phone").value;
+    const date=document.getElementById("date").value;
+    const time=document.getElementById("time").value;
+    console.log(`Name is ${name}\t Email is ${email}\tPhone is ${phone}\t`);
+}
